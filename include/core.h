93561f3fc73ff1c58680bd3e4b29619baeb3123c
wen.jie <wenjie204811@gmail.com>
2024-03-21 15:40:54 +0800

    目录结构调整

---
+++ include/core.h
@@ -0,0 +1,12 @@
+//
+// Created by wj on 24-3-21.
+//
+
+#ifndef DRINK_CORE_H
+#define DRINK_CORE_H
+
+extern char * rootDir;
+
+char * readFile(const char * sourceFile);
+
+#endif //DRINK_CORE_H
---
+++ vm/core.c
@@ -0,0 +1,13 @@
+//
+// Created by wj on 24-3-21.
+//
+
+#include "core.h"
+
+//根目录
+char * rootDir = NULL;
+
+char * readFile(const char * path)
+{
+    FILE * file = fopen(path, "r");
+}
